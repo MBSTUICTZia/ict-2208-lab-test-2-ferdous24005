6f9796ae65b1f0a1bb7877fe7842846724a0f7b8
ferdous24005 <it24005@mbstu.ac.bd>
2026-06-15 10:27:14 +0600

    ShapeMain.java

--- src/shapes/ShapeMain.java
+++ src/shapes/ShapeMain.java
@@ -1,23 +1,20 @@
 public class ShapeMain {
     public static void main(String[] args) {
-        // Create Rectangle and Circle objects
+
         Rectangle rect = new Rectangle("Red", true, 4.0, 5.0);
         Circle circle = new Circle("Blue", false, 3.0);
 
-        // Print objects using toString()
+
         System.out.println(rect);
         System.out.println();
         System.out.println(circle);
         System.out.println();
 
-        // Demonstrate downcasting
         System.out.println("--- Downcast Check ---");
-        
-        // Upcasting to Shape references
+
         Shape shape1 = rect;
         Shape shape2 = circle;
 
-        // Downcasting back to Rectangle and Circle
         if (shape1 instanceof Rectangle) {
             Rectangle downcastRect = (Rectangle) shape1;
             System.out.println("Rectangle width=" + downcastRect.getWidth() + 
@@ -29,4 +26,4 @@ public class ShapeMain {
             System.out.println("Circle radius=" + downcastCircle.getRadius());
         }
     }
-}// Enter code here
+}
